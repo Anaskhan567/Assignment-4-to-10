547c06147456a95f067cff43be65bc4d429f395e
Anaskhan567 <ma3799372@gmail.com>
2024-06-13 18:09:59 -0700

    Add files via upload

---
+++ Assignment 4 to 10/main.js
@@ -0,0 +1,40 @@
+// // Assignment no 4
+// let famquote:string ="a person never made a mistake never tried anything new";
+// let author: string = "Albert Einstein";
+// console.log(`${authorsaid} once said, "${famousquote}"`);
+// // Assignment no 5
+// let famousperson:string="Albert Einstein";
+// let message:string = `${famperson} once said, "${famousquote}"`;
+// console.log(msg);
+// Assignment no 6
+// let namewithwhitespace: string =  "\t \n Anas Khan \t \n "
+// console.log("namw with white space:", namewithwhitespace);
+// lat strippedName:string= namewithwhitespace.trim();
+// console.log("stripped Nmae:", strippedName);
+// Assigment no 7
+// let Addition: number= 5+3;
+// console.log(Addition);
+// let subtraction:number= 10-2;
+// console.log(subtraction);
+// let multiplacation:number=4*2;
+// console.log(multiplacation);
+// let division:number=16/2;
+// console.log(division);
+// // Assignment no 8
+// console.log(5+3);
+// console.log(10-2);
+// console.log(4*2);
+// console.log(16/2);
+// Assignment no 9
+var favoritedresult = 8;
+var message = "My Favorite number: ".concat(favoritedresult);
+console.log(message);
+// Assignment no 10
+// this program adds two number and prints the result
+//  define two number
+var number1 = 30;
+var number2 = 50;
+// Add two number
+var result = number1 + number2;
+// print result
+console.log(result);
